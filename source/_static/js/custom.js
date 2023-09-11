5c1344c9de9fbe9d01e7588722a618148b4da325
Max Pumperla <max.pumperla@googlemail.com>
2023-09-11 12:50:28 +0200

    style and sources
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

--- source/_static/js/custom.js
+++ source/_static/js/custom.js
@@ -11,15 +11,27 @@ var chatPopupDiv = document.createElement("div");
 chatPopupDiv.className = "chat-popup";
 chatPopupDiv.id = "chatPopup";
 chatPopupDiv.innerHTML = `
-    <div class="chatHeader bg-light p-2">
-        Ray Docs AI - Ask a question
-        <button id="closeChatBtn">x</button>
+    <div class="chatHeader bg-light p-2 d-flex justify-content-between align-items-center">
+        <div style="width: 30px;"></div>
+        <div class="text-center w-100">
+            <b>Ray Docs AI - Ask a question</b>
+        </div>
+        <button id="closeChatBtn" class="btn">
+            <i class="fas fa-times"></i>
+        </button>
     </div>
-    <div>
-        <input type="text" placeholder="Search">
-        <button id="searchBtn">Ask AI</button>
+    <div class="chatContentContainer">
+        <div class="input-group">
+            <input id="searchBar" type="text" class="form-control" placeholder="Search">
+            <div class="input-group-append">
+                <button id="searchBtn" class="btn btn-primary">Ask AI</button>
+            </div>
+        </div>
+        <div id="result"></div>
     </div>
-    <div id="result"></div>
+    <div class="chatFooter text-right p-2">
+        © Copyright 2023, The Ray Team.
+    </div>
 `
 document.body.appendChild(chatPopupDiv);
 
@@ -39,9 +51,10 @@ document.getElementById('closeChatBtn').addEventListener('click', function() {
 document.getElementById('searchBtn').addEventListener('click', function() {
     console.log('Form submitted');
     
+    var searchBar = document.getElementById('searchBar')
     var searchTerm = searchBar.value;
 
-    var resultDiv = getElementById('result')
+    var resultDiv = document.getElementById('result')
     resultDiv.textContent = ''; // Clear previous result
     resultDiv.textContent = `
         Processing your question... please wait 10-15 seconds.
@@ -64,14 +77,25 @@ document.getElementById('searchBtn').addEventListener('click', function() {
         console.log('Data:', data);
         resultDiv.textContent = '';
 
-        // Simulate streaming effect
+        // Streaming effect
         var text = data["answer"];
         var i = 0;
         function typeWriter() {
             if (i < text.length) {
                 resultDiv.textContent += text.charAt(i);
                 i++;
-                setTimeout(typeWriter, 10);
+                setTimeout(typeWriter, 5);
+            }
+            else {
+                resultDiv.innerText += "\n\nSources:\n\n"
+                const ul = document.createElement('ul');
+                const sources = data["sources"];
+                sources.forEach(item => {
+                    const li = document.createElement('li');
+                    li.innerHTML = `<a href="${item}" target="_blank">${item}</a>`;
+                    ul.appendChild(li);
+                });
+                resultDiv.appendChild(ul);
             }
         }
         typeWriter();
